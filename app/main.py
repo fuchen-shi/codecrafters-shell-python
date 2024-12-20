4a0018b74102ee72a2ff62de4c4d39f25db086fe
Fuchen Shi <fuchen@emq.com>
2024-12-20 12:24:20 +0800

    implement type

--- app/main.py
+++ app/main.py
@@ -1,23 +1,42 @@
 import sys
 
 
+class Builtins:
+    def _exit(args):
+        exit()
+
+    def _echo(args):
+        print(' '.join(args[1:]))
+
+    def _type(args):
+        command = args[1]
+        if command in Builtins.builtins:
+            print(f"{command} is a shell builtin")
+        else:
+            print(f"{command}: not found")
+
+    builtins = {
+        'exit': _exit,
+        'echo': _echo,
+        'type': _type,
+    }
+
+
+def handle_not_found(args):
+    command = args[0]
+    print(f"{command}: command not found")
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
 
-        # Wait for user input
-        command = input()
-        command_list = command.split()
-        program = command_list[0]
-        args = command_list[1:]
+        user_input = input()
+        args = user_input.split()
+        command = args[0]
 
-        match program:
-            case 'exit':
-                return
-            case 'echo':
-                print(' '.join(args))
-            case _:
-                print(f"{command}: command not found")
+        program = Builtins.builtins.get(command, handle_not_found)
+        program(args)
 
 
 if __name__ == "__main__":
