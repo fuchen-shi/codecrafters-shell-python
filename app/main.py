39163b035a0b0a5690242de8b5cc07ace7b6fd4d
Fuchen Shi <fuchen@emq.com>
2024-12-20 14:33:39 +0800

    refactor

--- app/main.py
+++ app/main.py
@@ -4,16 +4,6 @@ import subprocess
 from pathlib import PurePath
 
 
-def get_command_path(command):
-    path_env = os.environ["PATH"]
-    path_list = path_env.split(':')
-    for path in path_list:
-        command_path = PurePath(path, command).as_posix()
-        if os.path.isfile(command_path):
-            return command_path
-    return None
-
-
 class Builtins:
     def _exit(args):
         exit()
@@ -24,10 +14,11 @@ class Builtins:
     def _type(args):
         command = args[1]
 
-        if command in Builtins.builtins:
+        builtin = Builtins.builtins.get(command)
+        if builtin:
             print(f"{command} is a shell builtin")
             return
-        
+
         command_path = get_command_path(command)
         if command_path:
             print(f"{command} is {command_path}")
@@ -42,25 +33,32 @@ class Builtins:
     }
 
 
-def handle_not_found(args):
-    command = args[0]
-    print(f"{command}: command not found")
+def get_command_path(command):
+    PATH = os.environ["PATH"]
+    path_list = PATH.split(':')
+
+    for path in path_list:
+        command_path = PurePath(path, command).as_posix()
+        if os.path.isfile(command_path):
+            return command_path
+
+    return None
 
 
 def run_program(args):
     command = args[0]
 
-    if command in Builtins.builtins:
-        program = Builtins.builtins[command]
-        program(args)
+    builtin = Builtins.builtins.get(command)
+    if builtin:
+        builtin(args)
         return
-    
+
     command_path = get_command_path(command)
     if command_path:
         subprocess.run([command_path] + args[1:])
         return
-    
-    handle_not_found(args)
+
+    print(f"{command}: command not found")
 
 
 def main():
@@ -69,7 +67,7 @@ def main():
 
         user_input = input()
         args = user_input.split()
-        
+
         run_program(args)
 
 
