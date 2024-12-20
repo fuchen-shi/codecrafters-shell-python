5d2b49e6ac5de12f75db0f1f006a14e5eb9adabe
Fuchen Shi <fuchen@emq.com>
2024-12-20 11:05:53 +0800

    handle invalid commands

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    user_input = input()
+
+    print(f"{user_input}: command not found")
 
 
 if __name__ == "__main__":
